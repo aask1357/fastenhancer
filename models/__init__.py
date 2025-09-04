c7ba2a856403909812d2b1166664a369966fbf49
shahn14 <aask1357@naver.com>
2025-09-04 17:21:21 +0900

    bug fix

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-import importlib
-from .modelwrapper import AudioModelWrapper
-
-
-def get_wrapper(model : str) -> AudioModelWrapper:
-    module = importlib.import_module(f"models.{model}")
-    Wrapper = module.ModelWrapper
-    return Wrapper
